ce41dfb1e238a980b617be047bd31fec2e516d1f
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-10-17 15:00:29 +0545

    button component style @apply done

--- components/buttonComponent.vue
+++ components/buttonComponent.vue
@@ -50,11 +50,39 @@
     // let buttonColor = `bg-${props.color}-${props.colorNum}`
     let height = props.fullHeight ? `h-${props.fullHeight}` : '';
     let Width = props.fullWidth ? `w-${props.fullWidth}` : '';
-    let type = props.type === 'primary' ? 'bg-sky-500 hover:bg-sky-600 drop-shadow-sm' : props.type === 'secondary' ? 'bg-gray-500 hover:bg-gray-600 drop-shadow-sm' : props.type === 'success' ? 'bg-green-500 hover:bg-green-600 drop-shadow-sm' : props.type === 'danger' ? 'bg-red-500 hover:bg-red-600 drop-shadow-sm' : props.type === 'warning' ? 'bg-yellow-500 hover:bg-yellow-600 drop-shadow-sm' : props.type === 'info' ? 'bg-blue-400 hover:bg-blue-500 drop-shadow-sm' : props.type === 'light' ? 'bg-white' : props.type === 'dark' ? 'bg-black text-white' : props.type === 'link' ? 'bg-white text-blue-600 underline underline-offset-1' : `bg-${props.color}-${props.colorNum}`;
+    let type = props.type === 'primary' ? 'primary' : props.type === 'secondary' ? 'secondary' : props.type === 'success' ? 'success' : props.type === 'danger' ? 'danger' : props.type === 'warning' ? 'warning' : props.type === 'info' ? 'info' : props.type === 'light' ? 'light' : props.type === 'dark' ? 'dark' : props.type === 'link' ? 'link' : `bg-${props.color}-${props.colorNum}`;
     return `${round} ${type} ${height} ${Width}`; 
   };
 </script>
-
+<style>
+.primary{
+  @apply bg-sky-500 hover:bg-sky-600 drop-shadow-sm
+}
+.secondary{
+  @apply bg-gray-500 hover:bg-gray-600 drop-shadow-sm
+}
+.success{
+  @apply bg-green-500 hover:bg-green-600 drop-shadow-sm
+}
+.danger{
+  @apply bg-red-500 hover:bg-red-600 drop-shadow-sm
+}
+.warning{
+  @apply bg-yellow-500 hover:bg-yellow-600 drop-shadow-sm
+}
+.info{
+  @apply bg-blue-400 hover:bg-blue-500 drop-shadow-sm
+}
+.light{
+  @apply bg-white
+}
+.dark{
+  @apply bg-black text-white
+}
+.link{
+  @apply bg-white text-blue-600 underline underline-offset-1
+}
+</style>
 <!-- <script setup>
 import { h } from 'vue'
 render(()=>{
