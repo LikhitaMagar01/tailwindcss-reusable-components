4415e2dc6b71228da6e5bc686ef6b86f487b90f7
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-10-18 11:07:53 +0545

    spinner start

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,8 @@
         <div class="text-blue-300 underline underline-offset-1">
            <NuxtLink to="modal">Modal</NuxtLink> 
         </div>
+        <div class="text-blue-300 underline underline-offset-1">
+           <NuxtLink to="spinner">Spinner</NuxtLink> 
+        </div>
     </div>
 </template>
---
+++ pages/spinner/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import spinner from '@/components/spinner.vue'
+</script>
+
+<template>
+    <spinner />
+</template>
