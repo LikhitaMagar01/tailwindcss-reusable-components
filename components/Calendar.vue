70e4f51e648cc0cfcde508713ac787b8256fde3b
BinitaSubedi <subedibinita94@gmail.com>
2022-11-09 11:50:28 +0545

    added components

---
+++ components/Calendar.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="container mx-auto mt-5">
+  <div date-rangepicker class="flex items-center">
+  <div class="relative">
+   <div class="flex absolute inset-y-0 right-2 items-center pl-3 pointer-events-none">
+    <icon class="right-0" size="20" path="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1h2v2h6V1h2v2zm3 8H4v8h16v-8zm-5-6H9v2H7V5H4v4h16V5h-3v2h-2V5zm-9 8h2v2H6v-2zm5 0h2v2h-2v-2zm5 0h2v2h-2v-2z"></icon>
+   </div>
+   <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start">
+  </div>
+  <div class="relative ml-5">
+   <div class="flex absolute inset-y-0 right-2 items-center pl-3 pointer-events-none">
+    <icon class="right-0" size="20" path="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1h2v2h6V1h2v2zm3 8H4v8h16v-8zm-5-6H9v2H7V5H4v4h16V5h-3v2h-2V5zm-9 8h2v2H6v-2zm5 0h2v2h-2v-2zm5 0h2v2h-2v-2z"></icon>
+   </div>
+   <input name="start" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date end">
+  </div>
+ </div>
+ </div>
+</template>
+
