da9d08319cf13fd8b61511f5314ac39a9bed5aea
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-11-09 10:05:33 +0545

    datepicker ui

--- components/autocomplete.vue
+++ components/autocomplete.vue
@@ -2,10 +2,10 @@
 let emit = defineEmits(["input"]);
 
 var state = ref("");
-var places = ref("")
-var thisPlaces = ref("")
+var places = ref("");
+var thisPlaces = ref("");
 var filteredStates = ref([]);
-var filterState = ref(false)
+var filterState = ref(false);
 var states = ref([]);
 var noFilteredState = ref(false);
 
@@ -26,35 +26,35 @@ const props = defineProps({
     default: 0,
   },
   multiple: {
-    type: Boolean
+    type: Boolean,
   },
 });
 
 function filterStates() {
-  noFilteredState.value = false
-  filterState.value = true
-    filteredStates.value = props.options.filter((place) => {
+  noFilteredState.value = false;
+  filterState.value = true;
+  filteredStates.value = props.options.filter((place) => {
     return place.toLowerCase().startsWith(state.value.toLowerCase());
-  })
+  });
 }
 
 function setState(place) {
-  if(props.multiple){
+  if (props.multiple) {
     places.value = place;
-    thisPlaces.value = places.value.substring(0,5)
-    states.value.push(thisPlaces.value)
-    filterState.value = false
-    }else{
-      state.value = place;
-    }
+    thisPlaces.value = places.value.substring(0, 5);
+    states.value.push(thisPlaces.value);
+    filterState.value = false;
+  } else {
+    state.value = place;
+  }
 }
 
-function getList(){
-  noFilteredState.value = true
+function getList() {
+  noFilteredState.value = true;
 }
 
-function removeState(index){
-  states.value.splice(index, 1)
+function removeState(index) {
+  states.value.splice(index, 1);
 }
 </script>
 
@@ -71,37 +71,52 @@ function removeState(index){
 }
 </style> -->
 
-
 <template>
   <div class="m-10">
     <div
-      class="grid grid-row-2 p-1 w-80 bg-black drop-shadow-md text-white text-left relative rounded-md outline-sm  overflow-hidden"
+      class="grid grid-row-2 p-1 w-80 bg-black drop-shadow-md text-white text-left relative rounded-md outline-sm overflow-hidden"
       :tabindex="tabindex"
     >
-    <div class="flex flex-col flex-col-reverse w-80">
-    <div class="flex w-full flex flex-row-reverse bg-black text-white overflow-x-auto">
-      <span v-for="(st, index) in states" :key="st.id" class="text-white bg-black flex first:mr-auto min-w-max">
+      <div class="flex flex-col flex-col-reverse w-80">
+        <div
+          class="flex w-full flex flex-row-reverse bg-black text-white overflow-x-auto"
+        >
+          <span
+            v-for="(st, index) in states"
+            :key="st.id"
+            class="text-white bg-black flex first:mr-auto min-w-max"
+          >
             {{ st }}
-            <icon size="20" path="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" @click="removeState(index)"></icon>
-      </span>
+            <icon
+              size="20"
+              path="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+              @click="removeState(index)"
+            ></icon>
+          </span>
+        </div>
+        <input
+          type="text"
+          :placeholder="placeholder"
+          class="flex w-full place-items-center justify-between bg-black text-white"
+          v-model="state"
+          @click="getList()"
+          @input="filterStates()"
+        />
       </div>
-      <input
-        type="text"
-        :placeholder="placeholder"
-        class="flex w-full place-items-center justify-between bg-black text-white"
-        v-model="state"
-        @click="getList()"
-        @input="filterStates()"
-      />
-    </div>
       <div>
-        <ul v-if="noFilteredState" class="bg-black p-1  w-full top-11 rounded-md">
+        <ul
+          v-if="noFilteredState"
+          class="bg-black p-1 w-full top-11 rounded-md"
+        >
           <li v-for="option in options" @click="setState(option)">
             {{ option }}
           </li>
         </ul>
-        <ul v-if="filterState" class="bg-black p-1  w-full top-11 rounded-md">
-          <li v-for="filteredState in filteredStates" @click="setState(filteredState)">
+        <ul v-if="filterState" class="bg-black p-1 w-full top-11 rounded-md">
+          <li
+            v-for="filteredState in filteredStates"
+            @click="setState(filteredState)"
+          >
             {{ filteredState }}
           </li>
         </ul>
--- components/datepicker.vue
+++ components/datepicker.vue
@@ -1,39 +1,125 @@
 <script setup>
-const props = defineProps({
-  value: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  placeholder: {
-    type: String,
-    default: "",
-  },
-  type: {
-    type: String,
-    default: "text",
-  },
-  id: {
-    type: String,
-    default: "",
-  },
-  name: {
-    type: String,
-    default: "",
-  },
-});
+// const props = defineProps({
+//   value: {
+//     type: String,
+//     required: false,
+//     default: "",
+//   },
+//   placeholder: {
+//     type: String,
+//     default: "",
+//   },
+//   type: {
+//     type: String,
+//     default: "text",
+//   },
+//   id: {
+//     type: String,
+//     default: "",
+//   },
+//   name: {
+//     type: String,
+//     default: "",
+//   },
+// });
+var date = ref("")
+var month = ref()
+var todayIs = ref("")
+var year = ref("")
+var day = ref("")
+var letShow = ref(false)
+
+const nextMonth = ()=>{
+  var nextmonth = Number(month.value) + 1
+  console.log(typeof(nextmonth))
+  console.log(nextmonth)
+}
+
+const openTab = ()=>{
+  letShow.value = !letShow.value
+  var today = new Date();
+  var dt = today.getDate();
+  var mm = today.getMonth();
+  var dd = today.getDay();
+  var yyyy = today.getFullYear();
+  var today = mm + '/' + dd + '/' + yyyy;
+  todayIs.value = today
+  date.value = dt
+  year.value = yyyy
+  day.value = dd
+  console.log(month.value)
+  var getMonth = mm == "0" ? "January" : mm == "1" ? "February" : mm == "2" ? "March" : mm == "3" ? "April" : mm == "4" ? "May" : mm == "5" ? "June" : mm == "6" ? "July" : mm == "7" ? "August" : mm == "8" ? "September" : mm == "9" ? "October" : mm == "10" ? "November" : mm == "1" ? "December" : "invalid";
+  month.value = getMonth
+  var getDay = dd == "0" ? "Sunday" : dd == "1" ? "Monday" : dd == "2" ? "Tuesday" : dd == "3" ? "Wednesday" : dd == "4" ? "thursday" : dd == "5" ? "Friday" : dd == "6" ? "Saturday" : "Invalid"
+  day.value = getDay
+};
 </script>
 
 <template>
   <div>
-    <field
-      as="input"
-      :value="value"
-      class="border-2 border-black w-20 h-10"
-      :type="type"
-      :id="id"
-      :name="name"
-      :placeholder="placeholder"
-    />
+    {{letShow}}
+    <div class="relative w-1/4" @click="openTab()">
+      <input type="text" class="relative border border-2 border-black w-full h-7">
+      <span class="absolute right-2">
+        <icon class="" size="20" path="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1h2v2h6V1h2v2zm3 8H4v8h16v-8zm-5-6H9v2H7V5H4v4h16V5h-3v2h-2V5zm-9 8h2v2H6v-2zm5 0h2v2h-2v-2zm5 0h2v2h-2v-2z" />
+      </span>
+    </div>
+    <div>
+    </div>
+    <div :class="[letShow === false ? 'hidden' : 'static']" class="bg-white drop-shadow-md h-1/2 w-1/4">
+      <div >
+        <div class="flex flex-col-3">
+          <div class="">&lt;</div>
+        <div class="flex flex-col items-center">
+          {{month}}
+          {{year}}
+          {{day}}
+        </div>
+        <div class="" @click="nextMonth()">></div>
+        </div>
+        <ul class="grid grid-cols-7">
+          <li>SUN</li>
+          <li>MON</li>
+          <li>TUE</li>
+          <li>WED</li>
+          <li>THU</li>
+          <li>FRI</li>
+          <li>SAT</li>
+        </ul>
+        <ul class="grid grid-cols-7">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+          <li>13</li>
+          <li>14</li>
+          <li>15</li>
+          <li>16</li>
+          <li>17</li>
+          <li>18</li>
+          <li>19</li>
+          <li>20</li>
+          <li>21</li>
+          <li>22</li>
+          <li>23</li>
+          <li>24</li>
+          <li>25</li>
+          <li>26</li>
+          <li>27</li>
+          <li>28</li>
+          <li>29</li>
+          <li>30</li>
+          <li>31</li>
+        </ul>
+      </div>
+  </div>
   </div>
 </template>
