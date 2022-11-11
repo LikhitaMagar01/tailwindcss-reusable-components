f2baac388fbab87bb602dfec8d88af227e8c6a3f
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-11-11 12:53:58 +0545

    fileinput_component

---
+++ pages/fileInput/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FileInput from '~~/components/FileInput.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <!-- <FileDrop class="w-72 h-72" @upload="startUpload" /> -->
+    <file-input />
+    
+    </div>
+</template>
