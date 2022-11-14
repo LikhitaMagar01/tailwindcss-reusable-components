d6bf1e6a9116c5c8b07672fd4c9117a23ebaa97b
LikhitaMagar01 <likhita.magar@patancollege.edu.np>
2022-11-14 17:52:22 +0545

    transition remains

--- pages/drawer/index.vue
+++ pages/drawer/index.vue
@@ -5,8 +5,21 @@
 <template>
 <div>
     <Drawer side="left" dismiss>
-        <!-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam assumenda eligendi at praesentium. Quam perspiciatis ducimus labore necessitatibus optio vitae ipsum nostrum dolorum rem unde? Reprehenderit iste tenetur nemo ipsam?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus delectus rerum ad eos voluptates tenetur quod provident unde vitae vel. Eaque ea debitis delectus architecto natus ad cupiditate esse quod? -->
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam assumenda eligendi at praesentium. Quam perspiciatis ducimus labore necessitatibus optio vitae ipsum nostrum dolorum rem unde? Reprehenderit iste tenetur nemo ipsam?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus delectus rerum ad eos voluptates tenetur quod provident unde vitae vel. Eaque ea debitis delectus architecto natus ad cupiditate esse quod?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repellat at ratione saepe quidem quos culpa, non reprehenderit dolorum laudantium explicabo autem consectetur incidunt maiores modi eum odio nisi voluptatibus?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum ab suscipit amet officiis sunt quos itaque corrupti temporibus ut voluptatem optio quis neque sequi impedit, veniam cupiditate accusamus repudiandae accusantium?Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda doloremque expedita numquam quia sint similique veniam repudiandae voluptas autem eum totam quas, optio quam fugiat architecto sit commodi. Accusamus, animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, ab? Quos, non reiciendis laboriosam suscipit debitis aliquid commodi inventore, reprehenderit quo ea molestias quas, distinctio culpa quasi quaerat nulla in.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto repellendus laboriosam aut delectus illum vero reprehenderit! Necessitatibus laudantium quia vel, tempore suscipit magnam saepe cupiditate qui illum soluta ab voluptatibus?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores minus ullam, cupiditate ut, voluptas quos aliquid consequatur consectetur perferendis dicta deleniti reprehenderit voluptatibus commodi ipsum quaerat quae, soluta ducimus minima?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, ex. Excepturi nisi assumenda, dolor aliquam placeat fuga quod laborum aliquid nostrum consequatur ipsum aut odio explicabo molestiae rem voluptatibus facere?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum porro quos laudantium? Deleniti recusandae repellat rem exercitationem cum, ipsum dolor, nam sequi corrupti accusamus, error sint sed minima magnam quas.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quis. Et esse blanditiis dolorem sequi accusantium ratione eius cum fugit, placeat alias eaque modi repellat minima. Eum consectetur optio dolor?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aliquam? Aut ea cupiditate quisquam, ratione itaque porro! Fugiat soluta nostrum laboriosam nulla inventore beatae! Obcaecati sint mollitia recusandae facere voluptate.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, a? Totam animi quod molestias nulla id a, nam, dolorem ratione iste soluta explicabo delectus voluptatem illum earum obcaecati expedita distinctio!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit sed animi saepe consequuntur deserunt sit sequi! Voluptatibus nisi deleniti illo ullam tempore sapiente harum temporibus quia? Cupiditate expedita corporis vitae.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sed exercitationem quia, voluptates vel doloribus fugit iure amet fugiat rerum et, dignissimos deleniti id cum eaque eius reprehenderit? Provident, ex?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias esse, consequatur eaque qui illo earum natus cumque provident architecto repellat officia deleniti animi exercitationem quia voluptas est molestiae placeat maxime.
+
     </drawer>
 </div>
 </template>
